11412182942ceccfb9e1712b9133ad3e470ea176
Rrr <renaud.drieux@gmail.com>
2025-08-25 09:30:29 +0200

    ✨ Initial commit : version actuelle du Convertisseur

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 // babel.config.js
-module.exports = function (api) {
-  api.cache(true)
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-   
-    
-    ],
-  }
-}
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    'react-native-reanimated/plugin', // doit être le DERNIER
+  ],
+};
